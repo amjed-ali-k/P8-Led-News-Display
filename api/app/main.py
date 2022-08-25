73d0e216c37639f5657a1d8a26f8dcfc11a375d9
amjed-ali-k <amjedmgm@gmail.com>
2022-08-25 14:57:52 +0530

    remove cors

--- api/app/main.py
+++ api/app/main.py
@@ -6,15 +6,15 @@ from app.routes.base import app as route
 
 
 def get_application():
-    _app = FastAPI(title=settings.PROJECT_NAME)
+    _app = FastAPI(title="News API")
     _app.include_router(route)
-    _app.add_middleware(
-        CORSMiddleware,
-        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+    # _app.add_middleware(
+    #     CORSMiddleware,
+    #     allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+    #     allow_credentials=True,
+    #     allow_methods=["*"],
+    #     allow_headers=["*"],
+    # )
 
     return _app
 
